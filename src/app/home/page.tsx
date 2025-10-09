4e19ce44aaf96a54e183a1d9a4e937f1c936f3c0
SaraAlsukary <sarals6216@gmail.com>
2025-10-09 22:28:18 +0300

    add colors

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,8 +1,11 @@
+import Image from 'next/image'
 import React from 'react'
 
 const Home = () => {
     return (
-        <div className='landing'>Home</div>
+        <div className='landing'>
+            <Image width={1000} height={800} src={'/landing.jpg'} alt='landing'/>
+        </div>
     )
 }
 
