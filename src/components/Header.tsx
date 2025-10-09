87d335bc0fb23eb4cd0d2a92561607b287cb8b78
SaraAlsukary <sarals6216@gmail.com>
2025-10-09 23:37:56 +0300

    some changes

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -6,14 +6,14 @@ const Header = () => {
         <header className='p-10'>
             <div className='container flex justify-between items-center'>
                 <div className="logo text-white">
-                    <h1 className='font-bold text-2xl'>Digibits</h1>
+                    <h1 className='font-bold text-4xl'>Digibits</h1>
                 </div>
                 <nav>
                     <ul className='flex justify-center gap-7 items-center text-white'>
-                        <li><Link href={'/'}>Home</Link></li>
-                        <li><Link href={'/products'}>Products</Link></li>
-                        <li><Link href={'/about'}>About</Link></li>
-                        <li>
+                        <li className='text-xl'><Link href={'/'}>Home</Link></li>
+                        <li className='text-xl'><Link href={'/products'}>Products</Link></li>
+                        <li className='text-xl'><Link href={'/about'}>About</Link></li>
+                        <li className='text-xl'>
                             <Button>
                                 <Link href={'/contact'}>Contact</Link>
                             </Button>
